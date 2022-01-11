62b5772b1fe34f985c11f6cafc2c60e6b78b515c
Kerry Buckley <kerryjbuckley@gmail.com>
2022-01-11 09:20:24 +0000

    Remove boilerplate

--- lib/cheap_date.ex
+++ lib/cheap_date.ex
@@ -1,18 +1,2 @@
 defmodule CheapDate do
-  @moduledoc """
-  Documentation for `CheapDate`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> CheapDate.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/cheap_date_test.exs
+++ test/cheap_date_test.exs
@@ -1,8 +1,3 @@
 defmodule CheapDateTest do
   use ExUnit.Case
-  doctest CheapDate
-
-  test "greets the world" do
-    assert CheapDate.hello() == :world
-  end
 end
