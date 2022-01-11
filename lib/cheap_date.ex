24bce8a7995d2cafc671b77ed40ef2fb81b58486
Kerry Buckley <kerryjbuckley@gmail.com>
2022-01-11 10:26:07 +0000

    Implement ugly solution

--- lib/cheap_date.ex
+++ lib/cheap_date.ex
@@ -1,4 +1,13 @@
 defmodule CheapDate do
-  def choose_wine(_wines) do
+  def choose_wine(wines) do
+    if Enum.empty?(wines) do
+      nil
+    else
+      if length(wines) == 1 do
+        List.first(wines)
+      else
+        wines |> Enum.sort_by(& &1.price) |> Enum.at(1)
+      end
+    end
   end
 end
