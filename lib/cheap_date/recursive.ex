4c71af6efc40a655263f53ff8b355e6b9da2d6ed
Kerry Buckley <kerryjbuckley@gmail.com>
2022-01-12 17:09:35 +0000

    Add recursive version

---
+++ lib/cheap_date/recursive.ex
@@ -0,0 +1,20 @@
+defmodule CheapDate.Recursive do
+  def choose_wine([]), do: nil
+  def choose_wine([wine]), do: wine
+  def choose_wine(wines), do: do_choose_wine(wines, {%{price: nil}, %{price: nil}})
+
+  # Note: we rely on the fact that in Elixir nil compares as > any integer
+  defp do_choose_wine([], {_cheapest, second}), do: second
+
+  defp do_choose_wine([head | tail], {cheapest, _second}) when head.price < cheapest.price do
+    do_choose_wine(tail, {head, cheapest})
+  end
+
+  defp do_choose_wine([head | tail], {cheapest, second}) when head.price < second.price do
+    do_choose_wine(tail, {cheapest, head})
+  end
+
+  defp do_choose_wine([_head | tail], {cheapest, second}) do
+    do_choose_wine(tail, {cheapest, second})
+  end
+end
